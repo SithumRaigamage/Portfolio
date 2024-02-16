e41bf6c4cf3331c70ce8c86b10ddd2695d7d73c5
SithumRaigamage <sraig2002@gmail.com>
2024-02-16 20:17:28 +0530

    skills
    made the template of skills section but need to update with original details

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,11 @@
+const menuItem = document.getElementById('menu-item');
+const submenu = document.getElementById('submenu');
+const arrow = document.querySelector('.arrow');
 
+menuItem.addEventListener('click', () => {
+  submenu.classList.toggle('show');
+  arrow.classList.toggle('rotate');
+});
+
+
+  
