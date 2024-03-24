3b3bbc332cfafc32e4c558ca040d6e6751684786
SithumRaigamage <sraig2002@gmail.com>
2024-03-24 18:15:37 +0530

    Home
    HomeSection done

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,18 @@
-const menuItem = document.getElementById('menu-item');
-const submenu = document.getElementById('submenu');
-const arrow = document.querySelector('.arrow');
-
-menuItem.addEventListener('click', () => {
-  submenu.classList.toggle('show');
-  arrow.classList.toggle('rotate');
+document.addEventListener('DOMContentLoaded', function () {
+  var typedElement = document.querySelector(".heading");
+  if (typedElement) {
+      var typed = new Typed(typedElement, {
+          strings: ["Software Engineer Intern", "Software Engineering Undergraduate"],
+          typeSpeed: 100,
+          backSpeed: 100,
+          backDelay: 1000,
+          loop: true
+      });
+  } else {
+      console.error("Typed.js element not found.");
+  }
 });
+
 
 
   
