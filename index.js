aa5b02e97731a59ea3ef1f6cb004368cc04a1117
Sithum Raigamage <sithumraigamage@Sithums-MacBook-Air.local>
2025-02-02 14:59:33 +0530

    contact me section and format of document

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 // Function to filter portfolio items by category
 function filterPortfolio(category) {
-    const items = document.querySelectorAll('.portfolio-item');
-    
-    if (category === 'all') {
-        items.forEach(item => {
-            item.style.display = 'block'; // Show all items
-        });
-    } else {
-        items.forEach(item => {
-            if (item.classList.contains(category)) {
-                item.style.display = 'block'; // Show the selected category items
-            } else {
-                item.style.display = 'none'; // Hide the non-selected category items
-            }
-        });
-    }
+  const items = document.querySelectorAll(".portfolio-item");
+
+  if (category === "all") {
+    items.forEach((item) => {
+      item.style.display = "block"; // Show all items
+    });
+  } else {
+    items.forEach((item) => {
+      if (item.classList.contains(category)) {
+        item.style.display = "block"; // Show the selected category items
+      } else {
+        item.style.display = "none"; // Hide the non-selected category items
+      }
+    });
+  }
 }
 
 function scrollToContact() {
-    document.getElementById("contactme").scrollIntoView({ behavior: "smooth" });
-  }
-
+  document.getElementById("contactme").scrollIntoView({ behavior: "smooth" });
+}
